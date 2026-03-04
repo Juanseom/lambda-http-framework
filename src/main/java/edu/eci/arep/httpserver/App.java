72735931476041cf18d4bec6f768127f56be4c03
Juanseom <juanse.om13@gmail.com>
2026-03-03 22:19:37 -0500

    feat: finish point 2

--- src/main/java/edu/eci/arep/httpserver/App.java
+++ src/main/java/edu/eci/arep/httpserver/App.java
@@ -5,7 +5,7 @@ import static edu.eci.arep.httpserver.LambdaFramework.get;
 public class App {
 
     public static void main(String[] args) throws Exception {
-        get("/hello", (req, res) -> "Hello World!");
+        get("/hello", (req, res) -> "Hello " + req.getValues("name"));
 
         HttpServer.main(args);
     }
